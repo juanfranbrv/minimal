baf082265d86d5a3cea81a5a69c29294fddba919
juanfranbrv <juanfranbrv@gmail.com>
2025-02-17 01:32:14 +0100

    no-tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,9 @@ import { defineConfig } from "astro/config";
 
 import htmx from 'astro-htmx';
 
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [htmx()],
 
-  vite: {
-    plugins: [tailwindcss()]
-  }
 });
